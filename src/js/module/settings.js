e25d5a194155dfe7abf4f3eb41a7bd5c81687651
松本 和馬 <hello@mtmtkzm.com>
2017-06-17 23:54:47 +0900

    settings, stateをファイル分割

---
+++ src/js/module/settings.js
@@ -0,0 +1,4 @@
+export const FIELD_BLOCKS = document.querySelector('.field-block');
+export const N_HORIZONTAL = 12; // 横の数
+export const N_VERTICAL = 16; // 縦の数
+export const CELL_SIZE = 50; // px
--- src/js/module/state.js
+++ src/js/module/state.js
@@ -1,34 +1,7 @@
-const state = {
-  field: [],        // field squares
-                    // 0: ブロックなし, 1: 未確定,  2: 確定
-  blockType: '',
-  gameover: false,  // gameover flag
-  point: 0,         // 0 points
-  timelimit: 60000      // 1 min
-};
+import { FIELD_BLOCKS, N_HORIZONTAL, N_VERTICAL, CELL_SIZE } from './settings';
 
-export function setField (i, block) {
-  state.field[i] = block;
-
-  // ここで再描画する
+export default {
+  currentType: '',  // 現在のブロックの種別(そのままクラス名になる)
+  b: 13*N_HORIZONTAL + 2       // 基準となる場所（文字の左上のマス）
 }
 
-export function setBlockType(type) {
-  state.blockType = type;
-}
-
-export function get (path) {
-  // pathがStringだったら配列にする
-  if (!Array.isArray(path)) path = path.split();
-
-  // stateを直接書き換えないように
-  let object = state;
-  let index = 0;
-  let length = path.length;
-
-  while (object !== null && index < length) {
-    object = object[path[index++]];
-  }
-
-  return object;
-}
