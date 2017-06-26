e1db9b195f0c556b8387f74d5236e3cf5240bbe2
松本 和馬 <matsumoto_kazuma@liginc.co.jp>
2017-06-26 23:57:15 +0900

    add components

--- vue-ttrs/src/js/main.js
+++ vue-ttrs/src/js/main.js
@@ -1,11 +1,7 @@
 import Vue from 'vue'
-import App from './app'
+import App from './App.vue'
 
 new Vue({
   el: '#app',
   render: h => h(App)
 });
-
-if (process.env.NODE_ENV === 'development') {
-
-}
