3e3e7bba96032ab26ee7a20e3615deef9a92ea3a
松本 和馬 <matsumoto_kazuma@liginc.co.jp>
2017-06-27 02:33:52 +0900

    vueのコンパイルエラーを解消

--- vue-ttrs/src/js/components/App.vue
+++ vue-ttrs/src/js/components/App.vue
@@ -14,32 +14,3 @@
     }
   }
 </script>
-<style lang="scss">
-  #app {
-
-    h1 {
-      color: red;
-    }
-
-  }
-
-  :local {
-
-    .baz {
-      color: #f00;
-
-      p {
-        font-size: 20px;
-      }
-    }
-
-  }
-</style>
-<style lang="scss" scoped>
-  #app {
-    text-align: center;
-
-  }
-</style>
-
-
