33cf11ff49b416f239a910c43c62aae535be86e1
mtmtkzm <hello@mtmtkzm.com>
2017-07-02 22:12:11 +0900

    main jsでstoreを読み込み

--- vue-ttrs/src/js/components/Field.vue
+++ vue-ttrs/src/js/components/Field.vue
@@ -1,6 +1,27 @@
 <template>
-
+  <div class="field">
+    <div v-for="n in fieldArray"></div>
+  </div>
 </template>
 <script>
+  import {mapGetters} from 'vuex';
 
+  export default {
+    methods: {
+      initField() {
+        let field = [];
+        for (let i=0; i<10*15; i++) {
+          field.push(0);
+        }
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'fieldArray'
+      ])
+    },
+    created () {
+      this.initField();
+    }
+  }
 </script>
